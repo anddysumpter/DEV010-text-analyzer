ab807938bfe042febcf912c2d3181a5e0d10f450
Andrea Garcia <anddysumptermx@gmail.com>
2023-07-18 13:43:07 -0600

    actualizacion la cual genera ya menos errores en el test

--- src/analyzer.js
+++ src/analyzer.js
@@ -12,13 +12,17 @@ const analyzer = {
   getCharacterCountExcludingSpaces: (text) => {
     return text.replace(/\s/g, '').length;
   },
-  getAverageWordLength: (text) => {
-    const words = text.trim().split(/\s+/).filter(word => word !== '');
-    if (words.length === 0) {
-      return 0;
+  getAverageWordLength: (text) => {    
+    const wordL = text.split(" ");//la función split separa la cadena text con el espacio (" "), cada palabra o número se convierte en elemento en el array `wordL`
+    let lengthTotal = 0;//esta variable se utiliza para almacenar la suma de longitudes de las palabras
+    for (let i=0; i < wordL.length; i++){//el bucle for itera sobre cada elemento en el array `wordL`
+      lengthTotal += wordL[i].length;//Aqui se suma la longitud de cada palabra 
     }
-    const totalLength = words.reduce((sum, word) => sum + word.length, 0);
-    return totalLength / words.length;
+    const averageWord = lengthTotal / wordL.length; //se calcula la longitud promedio de las palabras, diviendo la suma total de longitudes entre la cantidad de palabras en el texto
+    const averageWordL = averageWord.toFixed(2);// con toFixed() redondeo a dos decimales el resultado
+    const averageWordFinal = Number(averageWordL);//como me daba un string se utilizo la funcion `Number` para convertirlo a número
+    return averageWordFinal;
+    
   },
   getNumberCount: (text) => {
     const numbers = text.match(/\b\d+(\.\d+)?\b/g);
